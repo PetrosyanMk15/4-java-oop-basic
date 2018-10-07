7c425f4a2661e9b4a53871a27dc2fa9297bed47c
Boris Tarelkin <batarelkin@chel.rusoft.local>
2018-10-07 20:56:18 +0500

    task01 test

--- task01/src/com/example/task01/Point.java
+++ task01/src/com/example/task01/Point.java
@@ -7,6 +7,26 @@ public class Point {
     int x;
     int y;
 
+    Point(int x, int y){
+        this.x = x;
+        this.y = y;
+    }
+
+    Point(){
+    }
+
+    void flip () {
+        int a = this.x;
+        this.x = -this.y;
+        this.y = -a;
+    }
+
+    double distance(Point p) {
+        double xProjection = p.x - this.x;
+        double yProjection = p.y - this.y;
+        return Math.sqrt(xProjection * xProjection + yProjection * yProjection);
+    }
+
     void print() {
         String pointToString = String.format("(%d, %d)", x, y);
         System.out.println(pointToString);
