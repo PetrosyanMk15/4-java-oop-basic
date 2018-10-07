88c46f2a92d36afda8cba497f851347d83c3d203
Leva <petrosian.lyova@yandex.ru>
2018-10-07 22:57:28 +0500

    Петросян. Task02

--- task02/src/com/example/task02/Task02Main.java
+++ task02/src/com/example/task02/Task02Main.java
@@ -2,6 +2,30 @@ package com.example.task02;
 
 public class Task02Main {
     public static void main(String[] args) {
+        TimeSpan t1 = new TimeSpan(3, 50, 33);
+        System.out.println(t1);
+
+        t1.setHours(15);
+        System.out.println(t1.getHours());
+
+        t1.setSeconds(20);
+        System.out.println(t1.getSeconds());
+
+        System.out.println(t1);
+
+        TimeSpan t2 = new TimeSpan();
+        t1.add(t2);
+        System.out.println(t1);
+
+        t2.setHours(3);
+        t2.setSeconds(15);
+        System.out.println(t2);
+
+        t1.add(t2);
+        System.out.println("Add: " + t1);
+
+        t1.subtract(t2);
+        System.out.println("Sub: " + t1);
 
     }
 }
---
+++ task02/src/com/example/task02/TimeSpan.java
@@ -0,0 +1,60 @@
+package com.example.task02;
+
+public class TimeSpan {
+    private int hours;
+    private int minutes;
+    private int seconds;
+
+    TimeSpan() {
+        hours = 0;
+        minutes = 0;
+        seconds = 0;
+    }
+
+    TimeSpan(int hours, int minutes, int seconds) {
+        this.hours = hours;
+        this.minutes = minutes;
+        this.seconds = seconds;
+    }
+
+    public int getHours() {
+        return this.hours;
+    }
+
+    public void setHours(int hours) {
+        this.hours = hours;
+    }
+
+    public int getMinutes() {
+        return this.minutes;
+    }
+
+    public void setMinutes(int minutes) {
+        this.minutes = minutes;
+    }
+
+    public int getSeconds() {
+        return this.seconds;
+    }
+
+    public void setSeconds(int seconds) {
+        this.seconds = seconds;
+    }
+
+    void add(TimeSpan time) {
+        this.hours += time.hours;
+        this.minutes += time.minutes;
+        this.seconds += time.seconds;
+    }
+
+    void subtract(TimeSpan time) {
+        this.hours -= time.hours;
+        this.minutes -= time.minutes;
+        this.seconds -= time.seconds;
+    }
+
+    public String toString() {
+        return "Hours: " + this.hours + "; minutes: " + this.minutes + "; seconds: " + this.seconds;
+    }
+
+}
