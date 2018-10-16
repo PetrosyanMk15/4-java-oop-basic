50f53f74f088746e6d3f1b9885e7b7efb5179e87
PetrosyanMk15 <43194832+PetrosyanMk15@users.noreply.github.com>
2018-10-16 20:37:01 +0500

    Update Point.java

--- task01/src/com/example/task01/Point.java
+++ task01/src/com/example/task01/Point.java
@@ -19,13 +19,13 @@ public class Point {
         this.y = y;
     }
 
-    void flip() {
+    public void flip() {
         int temp = this.x;
         this.x = -this.y;
         this.y = -temp;
     }
 
-    double distance(Point point) {
+    public double distance(Point point) {
         return Math.sqrt(Math.pow((double) (point.x - this.x), 2.0D) + Math.pow((double) (point.y - this.y), 2.0D));
     }
 
@@ -33,7 +33,7 @@ public class Point {
         return "(" + String.valueOf(this.x) + ", " + this.y + ")";
     }
 
-    void print() {
+    public void print() {
         String pointToString = String.format("(%d, %d)", this.x, this.y);
         System.out.println(pointToString);
     }
